aae4c6f5a099b10e221c5ef3aa893b190def9995
flzw1667 <flzw1667@th-wildau.de>
2024-10-25 17:06:20 +0200

    change

--- colab2.py
+++ colab2.py
@@ -185,7 +185,7 @@ def new_embedding(antigen_list, encoder):
         New embedded sequences and epitopes.
     """
     # Rekonstruktion der originalen Sequenzen mit dem Encoder
-    decoded_antigens = encoder.sequences_to_texts(antigen_list.tolist())
+    decoded_antigens = encoder.sequences_to_texts(antigen_list.tolist()).replace(" ", "")
     print("decoded_antigens: ", decoded_antigens[0], "decoded_antigens2: ", decoded_antigens[1])
 
     # Lade ESM-Tokenizer
