273d16ab0f0485b2026115d1b447112ea85e8845
flzw1667 <flzw1667@th-wildau.de>
2024-11-04 17:55:00 +0100

    TFEsmForTokenClassification

--- Ai_exec.py
+++ Ai_exec.py
@@ -196,7 +196,7 @@ def create_ai(filepath, save_file, output_file, train=False, safe=False,  valida
             #x = encoder_embed_out
             esm_model = TFEsmForTokenClassification.from_pretrained('facebook/esm2_t6_8M_UR50D')
             esm_model.trainable = True  # Stelle sicher, dass das Modell trainierbar ist
-            esm_outputs = esm_model(encoder_inputs, training=True, return_dict=False ) #['last_hidden_state'] # removed
+            esm_outputs = esm_model(encoder_inputs, training=True)['hidden_states ']
             x = esm_outputs
             print("Shape of esm_outputs:", esm_outputs.shape)
             output_dimension = esm_outputs.shape[2]
