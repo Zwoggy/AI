156877a273a0209cf984a1711e8e97ed6bcf1df0
flzw1667 <flzw1667@th-wildau.de>
2024-11-04 17:41:25 +0100

    Truncation turned back on in new_embeddign()

--- Ai_exec.py
+++ Ai_exec.py
@@ -196,7 +196,7 @@ def create_ai(filepath, save_file, output_file, train=False, safe=False,  valida
             #x = encoder_embed_out
             esm_model = TFEsmModel.from_pretrained('facebook/esm2_t6_8M_UR50D')
             esm_model.trainable = True  # Stelle sicher, dass das Modell trainierbar ist
-            esm_outputs = esm_model(x, training=True )['last_hidden_state']
+            esm_outputs = esm_model(encoder_inputs, training=True )['last_hidden_state']
             x = esm_outputs
             print("Shape of esm_outputs:", esm_outputs.shape)
             output_dimension = esm_outputs.shape[2]
