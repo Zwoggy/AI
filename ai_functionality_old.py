d037cbe3eab15df86842348f0a4bc9058f74578d
flzw1667 <flzw1667@th-wildau.de>
2024-12-22 19:27:11 +0100

    bug

--- ai_functionality_old.py
+++ ai_functionality_old.py
@@ -637,6 +637,7 @@ def modify_with_context(epitope_list, antigen_list, length_of_longest_sequence):
         context: defines the length after which the sequence will be cut if no epitope was found.
 
         returns the new antigen(actually protein) and epitope list aswell as the new length of the longest sequence to which every new sequence is padded."""
+    print(epitope_list, antigen_list)
     new_antigen_list: list = []
     new_epitope_list: list = []
     decoder_list: list = []
@@ -652,7 +653,6 @@ def modify_with_context(epitope_list, antigen_list, length_of_longest_sequence):
         for run, (aminoacid, char) in enumerate(zip(epitope, antigen)):
             i += 1
             if aminoacid == 1:
-                print("Yes2")
 
                 if start is True:
 
