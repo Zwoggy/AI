22dc8acf00c748a14a151d96b1c179a24d26a6c4
flzw1667 <flzw1667@th-wildau.de>
2025-03-12 17:37:10 +0100

    added auc to validation

--- ai_functionality_old.py
+++ ai_functionality_old.py
@@ -1481,7 +1481,7 @@ def evaluate_model(model, encoder, sequence, true_binary_epitope):
     # Da das Modell Wahrscheinlichkeiten ausgibt, runde auf 0 oder 1
     # print(true_binary_epitope)
     # print(predictions)
-    predicted_binary = np.where(predictions >= 0.5, 1, -1)
+    predicted_binary = np.where(predictions >= 0.6, 1, -1)
     # Berechne die Metriken
     auc = roc_auc_score(true_binary_epitope, predictions)
     recall = recall_score(true_binary_epitope, predicted_binary)
