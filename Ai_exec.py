8daf50573ca073a59a80468c3f6e9830957a6380
flzw1667 <flzw1667@th-wildau.de>
2025-05-01 11:21:57 +0200

    changed hyperparameter

--- Ai_exec.py
+++ Ai_exec.py
@@ -126,9 +126,9 @@ def create_ai(filepath, save_file, output_file, train=False, safe=False, validat
 
     num_transformer_blocks = 2 # used to be 2
     num_decoder_blocks = 1 # used to be 1
-    embed_dim = 160  # Embedding size for each token used to be 24
+    embed_dim = 80  # Embedding size for each token used to be 24
     num_heads = 40  # Number of attention heads; used to be 40
-    ff_dim = 256  # Hidden layer size in feed forward network inside transformer; used to be 32
+    ff_dim = 128  # Hidden layer size in feed forward network inside transformer; used to be 32
     maxlen = length_of_longest_context
     rate = 0.1
     training = True
