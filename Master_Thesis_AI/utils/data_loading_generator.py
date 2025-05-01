fa0a72fc4ae1a85cfa1b8734ad084caf9c860925
flzw1667 <flzw1667@th-wildau.de>
2025-05-01 12:01:03 +0200

    implemented data_generator

--- Master_Thesis_AI/utils/data_loading_generator.py
+++ Master_Thesis_AI/utils/data_loading_generator.py
@@ -26,21 +26,26 @@ class EpitopeDataGenerator(Sequence):
         return int(np.ceil(len(self.inputs) / self.batch_size))
 
     def __getitem__(self, index):
+        # Indizes für den aktuellen Batch
         batch_indices = self.indices[index * self.batch_size:(index + 1) * self.batch_size]
+
+        # Daten herausholen
         batch_inputs = self.inputs[batch_indices]
         batch_labels = self.labels[batch_indices]
 
-        # Entferne überflüssige Dimension (falls vorhanden)
+        # Falls Input-Shape (B, 1, L, D): Squeeze zweite Dimension
         if batch_inputs.ndim == 4 and batch_inputs.shape[1] == 1:
-            batch_inputs = batch_inputs[:, 0, :, :]  # (B, 1, L, D) → (B, L, D)
+            batch_inputs = np.squeeze(batch_inputs, axis=1)  # → (B, L, D)
 
+        # Optional: Sample Weights
         if self.sample_weights is not None:
             batch_weights = self.sample_weights[batch_indices]
             return batch_inputs, batch_labels, batch_weights
-        else:
-            return batch_inputs, batch_labels
+
+        return batch_inputs, batch_labels
 
     def on_epoch_end(self):
         # Mische die Daten am Ende jeder Epoche
         if self.shuffle:
             np.random.shuffle(self.indices)
+
