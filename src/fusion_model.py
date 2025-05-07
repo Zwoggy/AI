fc653b45f7af739619808dfc34652ec9b18cd269
flzw1667 <flzw1667@th-wildau.de>
2025-05-07 17:14:27 +0200

    removed_cnn_structure data padding

--- src/fusion_model.py
+++ src/fusion_model.py
@@ -74,7 +74,7 @@ def create_fusion_model_function(embed_dim, ff_dim, length_of_longest_context, m
 
 
     # Fusion block to fuse structural and sequential information together
-    decoder_outputs = keras.layers.Dropout(rate)(decoder_outputs)
+    decoder_outputs = keras.layers.Dropout(rate)(fused)
     decoder_outputs = keras.layers.Dense(12, activation='relu', name='Not_the_last_Sigmoid')(decoder_outputs)
 
     decoder_outputs = keras.layers.Lambda(lambda x: tf.identity(x))(decoder_outputs) # removes mask for timedistributed layer since it cant deal with a mask
