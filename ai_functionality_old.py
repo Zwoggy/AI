50194745f9b1dd6da459a96d648ab7a877096bee
flzw1667 <flzw1667@th-wildau.de>
2025-05-07 17:18:04 +0200

    removed_cnn_structure data padding

--- ai_functionality_old.py
+++ ai_functionality_old.py
@@ -643,7 +643,7 @@ def calculating_class_weights(y_true):
             weights[i] = w
 
     for i in range(len(weights)):
-        weights[i][1] = weights[i][1] / 0.8  # optional Scaling; used to be 2.2
+        weights[i][1] = weights[i][1] / 0.75  # optional Scaling; used to be 2.2
 
     return weights
 """
