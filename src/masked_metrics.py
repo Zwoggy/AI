370e76e3653f58bab2156f44826e87bf9af2b8cc
flzw1667 <flzw1667@th-wildau.de>
2025-05-22 13:46:46 +0200

    added old_dataset argument to parser args

--- src/masked_metrics.py
+++ src/masked_metrics.py
@@ -9,12 +9,16 @@ import keras
 
 def masked_accuracy(y_true, y_pred):
     mask = tf.cast(tf.not_equal(y_true, -1), tf.float32)
+    if tf.rank(mask) + 1 == tf.rank(y_pred):
+        mask = tf.expand_dims(mask, axis=-1)
     y_pred_bin = tf.cast(y_pred > 0.5, tf.float32)
     correct = tf.cast(tf.equal(y_true, y_pred_bin), tf.float12)
     return tf.reduce_sum(correct * mask) / tf.reduce_sum(mask + K.epsilon())
 
 def masked_precision(y_true, y_pred):
     mask = tf.cast(tf.not_equal(y_true, -1), tf.float32)
+    if tf.rank(mask) + 1 == tf.rank(y_pred):
+        mask = tf.expand_dims(mask, axis=-1)
     y_pred_bin = tf.cast(y_pred > 0.5, tf.float32)
     true_positives = tf.reduce_sum(tf.cast(y_pred_bin * y_true, tf.float32) * mask)
     predicted_positives = tf.reduce_sum(y_pred_bin * mask)
@@ -22,6 +26,8 @@ def masked_precision(y_true, y_pred):
 
 def masked_recall(y_true, y_pred):
     mask = tf.cast(tf.not_equal(y_true, -1), tf.float32)
+    if tf.rank(mask) + 1 == tf.rank(y_pred):
+        mask = tf.expand_dims(mask, axis=-1)
     y_pred_bin = tf.cast(y_pred > 0.5, tf.float32)
     true_positives = tf.reduce_sum(tf.cast(y_pred_bin * y_true, tf.float32) * mask)
     possible_positives = tf.reduce_sum(y_true * mask)
@@ -33,6 +39,8 @@ def masked_auc(y_true, y_pred):
 
     # Maske bestimmen
     mask = tf.not_equal(y_true, -1)
+    if tf.rank(mask) + 1 == tf.rank(y_pred):
+        mask = tf.expand_dims(mask, axis=-1)
 
     # Nur echte Werte
     y_true_masked = tf.boolean_mask(y_true, mask)
@@ -63,6 +71,8 @@ class MaskedAUC(tf.keras.metrics.AUC):
 
 def masked_f1_score(y_true, y_pred):
     mask = tf.cast(tf.not_equal(y_true, -1), tf.float32)
+    if tf.rank(mask) + 1 == tf.rank(y_pred):
+        mask = tf.expand_dims(mask, axis=-1)
     y_pred_bin = tf.cast(y_pred > 0.5, tf.float32)
 
     tp = tf.reduce_sum(y_pred_bin * y_true * mask)
