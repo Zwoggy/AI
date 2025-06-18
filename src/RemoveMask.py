67b402035809c0a62d6ad7cafc8c3c63bace1dd5
flzw1667 <flzw1667@th-wildau.de>
2025-06-18 19:20:25 +0200

    fixed paths

--- src/RemoveMask.py
+++ src/RemoveMask.py
@@ -3,3 +3,6 @@ import tensorflow as tf
 class RemoveMask(tf.keras.layers.Layer):
     def call(self, inputs):
         return inputs  # tf.identity optional
+
+    def compute_mask(self, inputs, mask=None):
+        return None  # ← das ist entscheidend!
