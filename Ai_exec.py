7a259627f7da9cb380a2ed2cf51776e9dbc142be
flzw1667 <flzw1667@th-wildau.de>
2025-06-26 12:49:40 +0200

    removed 4 FeedNN layers for testing purposes.

--- Ai_exec.py
+++ Ai_exec.py
@@ -625,15 +625,15 @@ def create_model_new(embed_dim, ff_dim, length_of_longest_context, maxlen, new_w
             num_heads=num_heads,
             dropout=rate
         )(decoder_outputs, encoder_outputs)
-    decoder_outputs = keras.layers.Dropout(rate)(decoder_outputs)
-    decoder_outputs = keras.layers.Dense(12, activation='relu', name='Not_the_last_Sigmoid')(decoder_outputs)
-    decoder_outputs = keras.layers.Dropout(rate)(decoder_outputs)
+    #decoder_outputs = keras.layers.Dropout(rate)(decoder_outputs)
+    #decoder_outputs = keras.layers.Dense(12, activation='relu', name='Not_the_last_Sigmoid')(decoder_outputs)
+    #decoder_outputs = keras.layers.Dropout(rate)(decoder_outputs)
 
-    decoder_outputs = keras.layers.Dense(8, activation='relu', name='Not_the_last_Sigmoid_02')(decoder_outputs)
-    decoder_outputs = keras.layers.Dropout(rate)(decoder_outputs)
+    #decoder_outputs = keras.layers.Dense(8, activation='relu', name='Not_the_last_Sigmoid_02')(decoder_outputs)
+    #decoder_outputs = keras.layers.Dropout(rate)(decoder_outputs)
 
-    decoder_outputs = keras.layers.Dense(8, activation='relu', name='Not_the_last_Sigmoid_03')(decoder_outputs)
-    decoder_outputs = keras.layers.Dropout(rate)(decoder_outputs)
+    #decoder_outputs = keras.layers.Dense(8, activation='relu', name='Not_the_last_Sigmoid_03')(decoder_outputs)
+    #decoder_outputs = keras.layers.Dropout(rate)(decoder_outputs)
 
     decoder_outputs = keras.layers.Dense(4, activation='relu', name='Not_the_last_Sigmoid_04')(decoder_outputs)
     """"
