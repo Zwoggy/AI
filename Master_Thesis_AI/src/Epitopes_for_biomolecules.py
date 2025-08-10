657340b1e05f2f8b7c985d88d1b4fe4181bf29d1
flzw1667 <flzw1667@th-wildau.de>
2025-08-10 15:38:43 +0200

    now with a proper epitope list in biomolecules_incl_sequences_and_epitopes.csv
    TODO: Needs to be verified

---
+++ Master_Thesis_AI/src/Epitopes_for_biomolecules.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import json
+
+
+
+
+
+
+
+
+
+
+
+
+
+def add_epitopes(input_csv, output_csv):
+    # Load CSV file
+    df = pd.read_csv(input_csv)
+    df.insert(0, "Binary Epitop", None)
+    for index, row in df.iterrows():
+        sequence_length = len(row["Sequence"])
+        binary_epitope_list = [int(0)] * sequence_length
+        current_positive_epitopes = row["Epitope"].split("+")
+        start_index = int(row["Starting_Index"])
+        for epitope in current_positive_epitopes:
+            epitope = int(epitope.strip())
+            pos = epitope - start_index
+            print("ID and pos: ", row["PDB ID"], pos)
+            binary_epitope_list[pos] = 1
+
+        df.loc[index, "Binary Epitop"] = json.dumps(binary_epitope_list)
+
+    df.to_csv(output_csv, index=False)
+
+
+
+if __name__ == "__main__":
+    input_csv = "C:/Users/fkori/PycharmProjects/AI/data/Caroll_et_al_data/biomolecules_incl_sequences.csv"
+    output_csv = "C:/Users/fkori/PycharmProjects/AI/data/Caroll_et_al_data/biomolecules_incl_sequences_and_epitopes.csv"
+    add_epitopes(input_csv, output_csv)
