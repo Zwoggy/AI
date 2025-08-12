8da20214998a3e782f1073f9e3dcb7fbe6a9d27c
flzw1667 <flzw1667@th-wildau.de>
2025-08-12 16:41:36 +0200

    maybe it works; now including the 29_external test set; epochs=1

--- Ai_exec.py
+++ Ai_exec.py
@@ -858,8 +858,10 @@ def load_and_evaluate_folds(X_test, X_train, checkpoint_filepath, fold, new_weig
     test_metrics = best_model.evaluate(X_test, y_test, batch_size=8, verbose="auto", return_dict=True)
 
     if twenty_nine_external_X is not None and twenty_nine_external_y is not None:
-        twenty_nine_external_X = np.asarray(twenty_nine_external_X, 'float32')
-        twenty_nine_external_y = np.asarray(twenty_nine_external_y, 'float32')
+        if twenty_nine_external_X is not None:
+            twenty_nine_external_X = np.asarray(twenty_nine_external_X, dtype=np.float32)
+        if twenty_nine_external_y is not None:
+            twenty_nine_external_y = np.asarray(twenty_nine_external_y, dtype=np.float32)
         print(type(twenty_nine_external_X), type(twenty_nine_external_y))
 
         print(twenty_nine_external_X.shape, twenty_nine_external_y.shape)
