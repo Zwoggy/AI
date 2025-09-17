445a9d0a7f19d4279042e2532166b69a47c42ce0
flzw1667 <flzw1667@th-wildau.de>
2025-09-17 12:40:05 +0200

    should save the model now

--- Ai_exec.py
+++ Ai_exec.py
@@ -493,10 +493,11 @@ def create_ai(filepath, save_file, output_file, train=False, safe=False, validat
 
         # plot_results(history)
 
-#
 
+        """
         tf_keras.utils.plot_model(model, expand_nested = True, show_shapes = True,
                                   to_file = './multi_model' + str("_test_") + '.png') #str("_test_") used to be str("i")
+        """
         if safe:
             save_ai(model, save_file, old=old)
         # load_model_and_do_stuff(testx_list, testy_list, model)
