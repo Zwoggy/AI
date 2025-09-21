edcfbf9027e7d2a93cc1b9b75fd228e9649dab74
flzw1667 <flzw1667@th-wildau.de>
2025-09-21 14:12:10 +0200

    added get_config() and @keras serialization dekorator to fix laoding RemoveMask

--- src/RemoveMask.py
+++ src/RemoveMask.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
+import keras
 
 #@tf.function
+@keras.saving.register_keras_serializable(package="Custom")
 class RemoveMask(tf.keras.layers.Layer):
     def call(self, inputs):
         return tf.identity(inputs)
@@ -12,3 +14,8 @@ class RemoveMask(tf.keras.layers.Layer):
     @classmethod
     def from_config(cls, config):
         return cls(**config)
+
+    def get_config(self):
+        # nothing to store, but required for serialization
+        base_config = super().get_config()
+        return base_config
