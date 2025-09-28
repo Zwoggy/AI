3a62ad1228e307fa63e234b6a2f6fdc282c5f8f3
nweise <nikolaj.weise@gmail.com>
2025-09-28 17:46:06 +0200

    refactor

--- Master_Thesis_AI/src/prediction_creator.py
+++ Master_Thesis_AI/src/prediction_creator.py
@@ -44,6 +44,8 @@ def use_model_and_predict_ma(threshold, test_run = False):
 
     # contains floats between 0 and 1
     predictions = model.predict(x_comb)
+
+    # contains csv data
     results = []
 
     # save results for every sequence
@@ -101,20 +103,20 @@ def get_weighted_loss(weights):
     return weighted_loss
 
 
-def create_better_heatmap(data, sequence, pdb_id):
+def create_better_heatmap(predictions, sequence, pdb_id):
     """Input: predictions from the model
     Output: Heatmaps according to the predictions for the whole sequence entered"""
     print("creating heatmap for pdb_id " + str(pdb_id))
 
-    data_list, sequence_list = create_blocks(data, sequence)
+    pred_list, sequence_list = create_blocks(predictions, sequence)
     # print(data_list, sequence_list)
-    data_list = np.reshape(data_list, (data_list.shape[1], data_list.shape[0]))
+    pred_list = np.reshape(pred_list, (pred_list.shape[1], pred_list.shape[0]))
     sequence_list = np.reshape(sequence_list, (sequence_list.shape[1], sequence_list.shape[0]))
     # print(data_list.shape)
 
     filename = OUTPUT_DIR + "heatmaps/" + str(pdb_id) + ".png"
     plt.figure(dpi = 1000)
-    sb.heatmap(data_list, xticklabels = False, yticklabels = False, vmin = 0.2, vmax = 0.8, cmap = "rocket_r", annot=sequence_list, fmt="")
+    sb.heatmap(pred_list, xticklabels = False, yticklabels = False, vmin = 0.2, vmax = 0.8, cmap = "rocket_r", annot=sequence_list, fmt="")
     plt.savefig(filename, dpi = 1000, bbox_inches = "tight")
     print("saved heatmap: " + filename)
 
@@ -145,7 +147,7 @@ def create_line_plot(predictions, true_epitope, pdb_id):
     plt.style.use('_mpl-gallery')
 
     fig, ax = plt.subplots(figsize=(12, 4))
-    #ax.plot(range(len(data)), data, linewidth=0.8) # Linie
+    #ax.plot(range(len(data)), data, linewidth=0.8) # line
     ax.scatter(range(len(predictions)), predictions, c=colors, s=20)  # Punkte, s = point size
     ax.set_ylim(0, 1)
     ax.set_xlabel("Index")
@@ -191,11 +193,11 @@ def evaluate_model(predictions, true_binary_epitope, threshold):
     return recall, precision, f1, mcc
 
 
-def create_pymol_heatmap_csv(data, pdb_id):
+def create_pymol_heatmap_csv(predictions, pdb_id):
     start_index = get_startingindex_by_pdbid(pdb_id)
 
     csv_data = []
-    for i, item in enumerate(data):
+    for i, item in enumerate(predictions):
         csv_data.append({
             'index': i + start_index,
             'prediction': item
